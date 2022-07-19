5d46cfeb27a79cd12bf2003e54adc53f6aa648a7
Xavski <xavioursaveski1@gmail.com>
2022-07-19 15:46:47 +1000

    replicate website

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function openMenu(){
-    document.body.classList += " menu__open"
+    document.body.classList += " menu--open"
 }
 
 function closeMenu(){
-    document.body.classList.remove('menu__open')
+    document.body.classList.remove('menu--open')
 }
